251f04a4c166c09a73f55539728093c5effd032f
Lida.Karimi <lida-karimi.masouleh@pohlcon.com>
2026-03-05 13:09:59 +0100

    Update Jenkinsfile & server-cmds.sh

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,11 +1,2 @@
-#!/usr/bin/env bash
-set -e
-
-source /etc/profile
-source ~/.bash_profile || true
-
-export IMAGE=$1
-cd /home/ec2-user
-docker-compose -f docker-compose.yaml down
 docker-compose -f docker-compose.yaml up --detach
 echo "success"
